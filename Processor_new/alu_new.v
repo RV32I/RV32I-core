fc3333d47a04b0938eb68a24871f917bb7139baa
sanan satti <sanansatti346@gmail.com>
2024-02-04 02:48:13 -0800

    i add this line

---
+++ Processor_new/alu_new.v
@@ -0,0 +1,88 @@
+//ALu
+module ALu
+#(
+	parameter [WIDTH-1:0]   WIDTH   =32
+)
+(
+
+	input 			      ALU_CONTROL,
+	input   [WIDTH-1:0]   oprend_1,
+	input   [WIDTH-1:0]   oprend_2,
+	input   [WIDTH-1:0]   alu_out,
+	output  [WIDTH-1:0]	  Alu_result,
+	output  [WIDTH-1:0]	  branch_execution
+); 
+
+reg                     insrt;
+reg   			        opcode;
+wire  [14:12]      Func3;
+wire  [31:25]      Func7;
+wire  [WIDTH-1:0]    	ADD		=	4'b0000 ;
+wire  [WIDTH-1:0]    	SUB		=	4'b0001 ;
+wire  [WIDTH-1:0]    	SLL		=	4'b0010 ;
+wire  [WIDTH-1:0]    	SLT		=	4'b0011 ;
+wire  [WIDTH-1:0]       SLTU	=	4'b0100 ;
+wire  [WIDTH-1:0]    	XOR		=	4'b0101 ;
+wire  [WIDTH-1:0]    	SRL		=	4'b0110 ;
+wire  [WIDTH-1:0]    	SRA		=	4'b0111 ;
+wire  [WIDTH-1:0]    	OR		=	4'b1000 ;
+wire  [WIDTH-1:0]    	AND		=	4'b1001 ;
+wire  [WIDTH-1:0]    	BEQ		=	4'b1010 ; 
+wire  [WIDTH-1:0]    	BNE		=	4'b1011 ; 
+wire  [WIDTH-1:0]    	BLT		=	4'b1100 ;	
+wire  [WIDTH-1:0]    	BGE		=	4'b1101 ; 
+wire  [WIDTH-1:0]    	BLTU	=	4'b1110 ; 
+wire  [WIDTH-1:0]    	BGEU	=	4'b1111 ;	
+
+
+always@* opcode = insrt[6:0];
+always@(*)
+begin 
+	case(ALU_CONTROL)
+		ADD    : alu_out = oprend_1 +  oprend_2;
+		SUB    : alu_out = oprend_1 -  oprend_2;
+		SLL	   : alu_out = oprend_1 << oprend_2;
+		SLT	   : alu_out = $signed (oprend_1) <  $signed (oprend_2);
+		SLTU   : alu_out = oprend_1 <  oprend_2; 
+		XOR    : alu_out = oprend_1 ^  oprend_2; 
+		SRL    : alu_out = oprend_1 >> oprend_2;
+		SRA    : alu_out = oprend_1 >>> oprend_2;
+		OR     : alu_out = oprend_1 |  oprend_2;
+		AND    : alu_out = oprend_1 &  oprend_2;
+		BEQ    : alu_out = oprend_1 ==  oprend_2;
+		BNE    : alu_out = oprend_1 <= oprend_2; 
+		BLT    : alu_out = $signed (oprend_1) < $signed (oprend_2);
+		BGE    : alu_out = $signed (oprend_1) >= $signed (oprend_2);
+		BLTU   : alu_out = oprend_1 < oprend_2;
+        BGEU   : alu_out = oprend_1 >= oprend_2; 
+	endcase	
+end
+
+
+
+assign ALU_CONTROL = (Func3 == 3'b000) ? ADD :
+					(Func3 == 3'b000) ? SUB :
+					(Func3 == 3'b001) ? SLL :
+					(Func3 == 3'b010) ? SLT :
+					(Func3 == 3'b011) ? SLTU:
+					(Func3 == 3'b100) ? XOR :
+					(Func3 == 3'b101) ? SRL : 
+					(Func3 == 3'b101) ? SRA :
+					(Func3 == 3'b110) ? OR  :
+					(Func3 == 3'b111) ? AND ; 
+					
+					
+assign branch_execution =   (Func3 == 3'b000) ? BEQ  :
+							(Func3 == 3'b001) ? BNE  :
+							(Func3 == 3'b100) ? BLT  :
+							(Func3 == 3'b101) ? BGE  :
+							(Func3 == 3'b110) ? BLTU :
+							(Func3 == 3'b111) ? BGEU ;
+	
+always@(*) 
+begin 
+	if (opcode == 7'b0110011)
+		alu_out = Alu_result;
+	else if (opcode == 7'b1100011)
+		alu_out = branch_execution;
+end
---
+++ Processor_new/processor_new.v
@@ -0,0 +1,178 @@
+module processor
+#(
+	parameter  RAM_WIDTH 		= 32,
+	parameter  RAM_DEPTH 		= 9,
+	parameter  PROG_FILE		="",
+	parameter  PROG_START_ADDR  =10,
+	parameter  PROG_END_ADDR    =10,
+	parameter  DATA_FILE        ="",
+	parameter  DATA_START_ADDR  =10,
+	parameter  DATA_END_ADDR    =10
+)
+(
+	input clk,
+	input reset
+);
+
+
+reg  		[RAM_WIDTH-1:0]     INSRT;
+reg  		[RAM_WIDTH-1:0]		PROG_ADDR_OUT;
+reg  		[RAM_WIDTH-1:0]		DM_OUT;
+reg  		[RAM_WIDTH-1:0]		REG_DATA_IN;
+reg  		[RAM_WIDTH-1:0]		ALU_OUT;
+reg			[RAM_WIDTH-1:0]		opr1;
+reg			[RAM_WIDTH-1:0]		opr2;
+reg			[RAM_WIDTH-1:0]		Reg_data1;
+reg			[RAM_WIDTH-1:0]		Reg_data2;
+reg			[RAM_WIDTH-1:0] 	prog_cnt;
+reg			[RAM_WIDTH-1:0] 	branch;
+reg			[RAM_WIDTH-1:0] 	j;
+reg			[RAM_WIDTH-1:0] 	pc4;
+reg			[RAM_WIDTH-1:0]		op1_sel;
+reg			[RAM_WIDTH-1:0]		op2_sel;
+wire 		[1:0] 				selpc;
+wire 		[1:0]			    WBSEL;
+wire        [1:0]				imm_sel;
+wire 		INSRT[31:12]		LUI;
+output reg  [PC_WIDTH-1:0] 		mux4t01;	
+
+//program counter 	
+
+always@(posedge clk)
+begin 
+	if (reset)
+		prog_cnt <= 0;
+	else
+		prog_cnt <= mux4t01;
+end
+
+always@(*)
+begin 
+	case(selpc)
+		2'b00   : mux4t01 = 
+		2'b01   : mux4t01 = j;
+		2'b10   : mux4t01 = branch;
+		default : mux4t01 = pc4;
+    endcase
+end
+
+always@* pc4 = prog_cnt + 4;
+
+
+//instruction_memory
+progam_memory
+#(
+	.RAM_WIDTH 			(RAM_WIDTH), 		
+    .RAM_ADDR_BITS 	    (RAM_DEPTH),
+    .PROG_FILE 		    (PROG_FILE), 	 		
+    .INIT_START_ADDR 	(PROG_START_ADDR), 	
+	.INIT_END_ADDR	    (PROG_END_ADDR)	
+)	
+inst_p_memory
+(
+	.clock			(clk),
+    .ram_enable		(1'b1),	
+	.write_enable	(1'b0),
+    .address		(prog_cnt),
+    .input_data		(0),	
+    .output_data	(INSRT)
+);
+
+//reg addr select mux
+always@* PROG_ADDR_OUT = WASEL ? 1 : INSRT;
+
+
+//register file 
+reg_file reg_file_inst
+(
+	.clk			(clk),
+    .add_1			(INSRT[19:15]),
+    .add_2			(INSRT[24:20]),
+    .write_en		(),
+    .write_addr		(PROG_ADDR_OUT),
+    .wr_data		(REG_DATA_IN),
+    .rd_data_1		(Reg_data1),
+    .rd_data_2		(Reg_data2)
+);	
+
+
+//data memory
+data_memory
+#(
+	.RAM_WIDTH 			(RAM_WIDTH), 		
+    .RAM_ADDR_BITS 	    (RAM_DEPTH),		
+	.DATA_FILE 		    (DATA_FILE),
+    .INIT_START_ADDR 	(DATA_START_ADDR), 	
+	.INIT_END_ADDR	    (DATA_END_ADDR)	
+)	
+inst_d_memory 
+(
+	.clock			(clk),
+    .ram_enable		(1'b1),	
+	.write_enable	(),
+    .address		(ALU_OUT),
+    .input_data		(Reg_data2),	
+    .output_data	(DM_OUT)
+);
+
+//register write back select mux
+always@(*)
+begin 
+	case(WBSEL)
+		2'b00 : REG_DATA_IN = pc4;
+		2'b01 : REG_DATA_IN = DM_OUT;
+		2'b10 : REG_DATA_IN = ALU_OUT;
+		2'b11 : REG_DATA_IN = LUI;
+		endcase
+end
+
+//Oprend1 select mux
+
+always@(*)
+begin 
+	case(op1_sel)
+		2'b00 : opr1 = pc4;
+		2'b01 : opr1 = INSRT[30:21];
+		2'b10 : opr1 = Reg_data1;
+end
+
+//oprend2 select mux
+
+always@(*)
+begin 
+	case(op2_sel
+		2'b00 : opr2 = Reg_data2;
+		2'b01 : opr2 = INSRT[31:12];
+		2'b10 : opr2 = Imm_out;
+end
+
+//Imm select mux
+
+always@(*)
+begin 
+	case(imm_sel)
+		2'b00 : Imm_out = INSRT[31:20];
+		2'b01 : Imm_out = INSRT[24:20];
+		2'b10 : Imm_out = INSRT[31:25][11:7];
+end
+
+// branch adder
+
+//always@* branch = pc4 + INSRT[31:25][11:7]; 
+always@* branch = pc4 + INSRT[31:25][11:7];
+
+//ALU
+
+ALU 
+#(
+
+)
+isnt_ALU
+(
+	.clk			(clk)
+    .reset			(reset)
+    .ALU_CONTROL	()
+    .oprend_1		(opr1)
+    .oprend_2		(opr2)
+    .alu_out		()
+)
